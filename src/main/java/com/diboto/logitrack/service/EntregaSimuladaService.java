85df976b3611aa81f34262c56bcc24896db9de89
ViniciusFrancaCroa46 <viniciusfrancacoviello@gmail.com>
2025-04-15 22:31:21 -0300

    Adicionando SLService

---
+++ src/main/java/com/diboto/logitrack/service/EntregaSimuladaService.java
@@ -0,0 +1,14 @@
+package com.diboto.logitrack.service;
+
+import com.diboto.logitrack.model.EntregaSimulada;
+import com.diboto.logitrack.repository.EntregaSimuladaRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EntregaSimuladaService {
+
+}
