976823362eaf2150c527c6064541b69817220b8e
ViniciusFrancaCroa46 <viniciusfrancacoviello@gmail.com>
2025-04-21 23:17:14 -0300

    Adicionando EntregaSimuladaService & EventoSensorialService

--- src/main/java/com/diboto/logitrack/service/EntregaSimuladaService.java
+++ src/main/java/com/diboto/logitrack/service/EntregaSimuladaService.java
@@ -10,5 +10,30 @@ import java.util.List;
 import java.util.Optional;
 
 public class EntregaSimuladaService {
+    @Autowired
+    private EntregaSimuladaService entregaSimuladaService;
 
+    public List<EntregaSimulada> listaEntregas() {
+        return repository.findAll();
+    }
+
+    public Optional<EntregaSimulada> buscaEntregaPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public EntregaSimulada salvaEntrega(EntregaSimulada entrega) {
+        return repository.save(entrega);
+    }
+
+    public boolean existeEntrega(Long id) {
+        return repository.existsById(id);
+    }
+
+    public boolean excluirEntrega(Long id) {
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
